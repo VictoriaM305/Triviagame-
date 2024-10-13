a79221c635314b8fa56af072eccdf3a655c81730
Victoria Mckinnie <Vmcki004@fiu.edu>
2024-10-12 21:12:17 -0400

    Initial Commit

---
+++ triviagame/triviagameApp.swift
@@ -0,0 +1,17 @@
+//
+//  triviagameApp.swift
+//  triviagame
+//
+//  Created by user on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct triviagameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
